f3c5ed335e04978429e4a1e6d92e814b0386e2bb
Filip Karcz <filipkarcz01@gmail.com>
2023-07-06 08:39:26 +0200

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Filip Karcz on 06/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
